0e7decd464d54ac414113823017a7b62ee3f192b
fernandes-draw <fernandes_draw@hotmail.com>
2025-10-29 12:31:06 -0300

    Create layout.page and configure index.css and app.css

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import LayoutPage from './LayoutPage';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<>Login Page</>} />
+        <Route path='' element={<LayoutPage />}>
+          <Route path='portal/admin' element={<>Manage Employee Page</>} />
+          <Route path='portal/admin/myself' element={<>Manage Self Page</>} />
+          <Route path='portal/manager' element={<>Manage Reports Page</>} />
+          <Route
+            path='portal/manager/timesheets'
+            element={<>Review Time Sheets Page</>}
+          />
+          <Route path='portal/manager/myself' element={<>Manage Self Page</>} />
+          <Route path='portal/employee' element={<>Manage Self Page</>} />
+          <Route
+            path='portal/employee/timesheets'
+            element={<>Time Sheet Page</>}
+          />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
